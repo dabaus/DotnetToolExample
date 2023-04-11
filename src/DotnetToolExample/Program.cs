016901f6ad29f39b2b80c1cdd0883a1ee9e5b246
Jakob Stengard <jstengard@Jakobs-MacBook-Pro.local>
2023-04-11 15:38:03 +0200

    Inital commit

---
+++ src/DotnetToolExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using CommandLine;   
+namespace DotnetToolExample;
+
+public static class Program
+{
+    public class Options
+    {
+        [Option('c', "cat", Required = false, HelpText = "Print a cat")]
+        public bool IsCatOpt { get; set; }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed<Options>(o =>
+            {
+                if (o.IsCatOpt)
+                {
+                    PrintCat();
+                }
+                else
+                {
+                    Console.WriteLine("This is an example app");
+                }
+            });
+    }
+
+    public static void PrintCat()
+    {
+        Console.WriteLine(" /\\_/\\");
+        Console.WriteLine("( o.o )");
+        Console.WriteLine(" > ^ <");
+    }
+}
